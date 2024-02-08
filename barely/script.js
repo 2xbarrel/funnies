9279579b282fc0b1e1dc657f6752412fe069355b
2xbarrel <barrelthegreat@gmail.com>
2024-02-08 11:28:32 +0300

    i wish i was dead

---
+++ barely/script.js
@@ -0,0 +1,9 @@
+let visual = document.getElementById("visual");
+let text = document.getElementById("text");
+let button = document.getElementById("button");
+
+function onclickevent(){
+  console.log("swagmonayyyyy"); //haha not removing console log from code go brrrrr
+  visual.innerHTML = "dsdkjfsdhlfjdshlkfdshlfjh";
+  text.innerHTML = "look im doing my best";
+};

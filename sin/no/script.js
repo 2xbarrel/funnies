8b7211abad740e010d15888267b7ae3dc4656626
2xbarrel <barrelthegreat@gmail.com>
2024-04-09 23:34:47 +0300

    sin

---
+++ sin/no/script.js
@@ -0,0 +1,357 @@
+const script = [
+  { //dialogue 1
+    "bg": "stanleytv",
+    "overlay": "trash",
+    "char1": {
+      "emotion": "IDLE",
+      "animation": "appear",
+      "name": "Kumani"
+    },
+    "char2": {
+      "emotion": "IDLE",
+      "name": "Ambrose"
+    },
+    "talker": "char1",
+    "say": "MAn i sure do love sitting at my house in peace."
+  },
+  { //dialogue 2
+    "bg": "stanleytv",
+    "overlay": "trash",
+    "char1": {
+      "emotion": "IDLE",
+      "name": "Kumani"
+    },
+    "char2": {
+      "emotion": "IDLE",
+      "animation": "appear",
+      "name": "Ambrose"
+    },
+    "talker": "char2",
+    "say": "Hey bb gurl darling bb how you doing on this fine day mmhaa ahahaha."
+  },
+  { //dialogue 3
+    "bg": "stanleytv",
+    "overlay": "trash",
+    "char1": {
+      "emotion": "CONFUSED",
+      "name": "Kumani"
+    },
+    "char2": {
+      "emotion": "IDLE",
+      "name": "Ambrose"
+    },
+    "talker": "char1",
+    "say": "The hell do you want?"
+  },
+  { //dialogue 4
+    "bg": "stanleytv",
+    "overlay": "trash",
+    "char1": {
+      "emotion": "CONFUSED",
+      "name": "Kumani"
+    },
+    "char2": {
+      "emotion": "IDLE",
+      "name": "Ambrose"
+    },
+    "talker": "char2",
+    "say": "sex please."
+  },
+  { //dialogue 5
+    "bg": "stanleytv",
+    "overlay": "trash",
+    "char1": {
+      "emotion": "CONFUSED",
+      "name": "Kumani"
+    },
+    "char2": {
+      "emotion": "IDLE",
+      "name": "Ambrose"
+    },
+    "talker": "char1",
+    "say": "no."
+  },
+  { //dialogue 6
+    "bg": "stanleytv",
+    "overlay": "trash",
+    "char1": {
+      "emotion": "CONFUSED",
+      "name": "Kumani"
+    },
+    "char2": {
+      "emotion": "ANGRY",
+      "name": "Ambrose"
+    },
+    "talker": "char1",
+    "say": "Thats it."
+  },
+  { //dialogue 7
+    "bg": "stanleytv",
+    "overlay": "trash",
+    "char1": {
+      "emotion": "CONFUSED",
+      "name": "Kumani"
+    },
+    "char2": {
+      "emotion": "ANGRY",
+      "name": "Ambrose"
+    },
+    "talker": "char1",
+    "say": "Im gonna use my SECRET TECHNIQUE."
+  },
+  { //dialogue 8
+    "bg": "stanleytv",
+    "overlay": "trash",
+    "char1": {
+      "emotion": "CONFUSED",
+      "name": "Kumani"
+    },
+    "char2": {
+      "emotion": "ANGRY",
+      "name": "Ambrose"
+    },
+    "talker": "char1",
+    "say": "why do you look like that?"
+  },
+  { //dialogue 9
+    "bg": "stanleytv",
+    "overlay": "trash",
+    "char1": {
+      "emotion": "CONFUSED",
+      "name": "Kumani"
+    },
+    "char2": {
+      "emotion": "IDLE",
+      "name": "Barl"
+    },
+    "talker": "char2",
+    "say": "Hey guys, quadrelino barrelino here"
+  },
+  { //dialogue 10
+    "bg": "stanleytv",
+    "overlay": "trash",
+    "char1": {
+      "emotion": "CONFUSED",
+      "name": "Kumani"
+    },
+    "char2": {
+      "emotion": "IDLE",
+      "name": "Barl"
+    },
+    "talker": "char2",
+    "say": "basically ambrose looks weird because maximum sprite width is around 100 pixels so i had to cram him down in a little box"
+  },
+  { //dialogue 11
+    "bg": "stanleytv",
+    "overlay": "trash",
+    "char1": {
+      "emotion": "CONFUSED",
+      "name": "Kumani"
+    },
+    "char2": {
+      "emotion": "IDLE",
+      "name": "Barl"
+    },
+    "talker": "char2",
+    "say": "and im lazy, you shouldve noticed that from re used background and overlay"
+  },
+  { //dialogue 12
+    "bg": "stanleytv",
+    "overlay": "trash",
+    "char1": {
+      "emotion": "CONFUSED",
+      "name": "Kumani"
+    },
+    "char2": {
+      "emotion": "IDLE",
+      "name": "Barl"
+    },
+    "talker": "char1",
+    "say": "seems kinda cheap, i mean it is cheap"
+  },
+  { //dialogue 13
+    "bg": "stanleytv",
+    "overlay": "trash",
+    "char1": {
+      "emotion": "CONFUSED",
+      "name": "Kumani"
+    },
+    "char2": {
+      "emotion": "IDLE",
+      "animation": "disappear",
+      "name": "Barl"
+    },
+    "talker": "char2",
+    "say": "shut the fuck up"
+  },
+  { //dialogue 14
+    "bg": "stanleytv",
+    "overlay": "trash",
+    "char1": {
+      "emotion": "CONFUSED",
+      "name": "Kumani"
+    },
+    "char2": {
+      "emotion": "ANGRY",
+      "animation": "appear",
+      "name": "Ambrose"
+    },
+    "talker": "char2",
+    "say": "Thats it prepare to be totally convinced"
+  },
+  { //dialogue 14
+    "bg": "stanleytv",
+    "overlay": "trash",
+    "char1": {
+      "emotion": "CONFUSED",
+      "name": "Kumani"
+    },
+    "char2": {
+      "emotion": "CRY",
+      "name": "Ambrose"
+    },
+    "talker": "char2",
+    "say": ""
+  },
+  { //dialogue 14
+    "bg": "stanleytv",
+    "overlay": "trash",
+    "char1": {
+      "emotion": "ANGRY",
+      "name": "Kumani"
+    },
+    "char2": {
+      "emotion": "CRY",
+      "name": "Ambrose"
+    },
+    "talker": "char1",
+    "say": "im not impressed"
+  },
+  { //dialogue 14
+    "bg": "stanleytv",
+    "overlay": "trash",
+    "char1": {
+      "emotion": "ANGRY",
+      "name": "Kumani"
+    },
+    "char2": {
+      "emotion": "CRY",
+      "name": "Ambrose"
+    },
+    "talker": "char2",
+    "say": ""
+  },
+  { //dialogue 14
+    "bg": "stanleytv",
+    "overlay": "trash",
+    "char1": {
+      "emotion": "ANGRY",
+      "name": "Kumani"
+    },
+    "char2": {
+      "emotion": "CRY",
+      "name": "Ambrose"
+    },
+    "talker": "char1",
+    "say": "ok you can stop that now"
+  },
+  { //dialogue 14
+    "bg": "stanleytv",
+    "overlay": "trash",
+    "char1": {
+      "emotion": "ANGRY",
+      "name": "Kumani"
+    },
+    "char2": {
+      "emotion": "CRY",
+      "name": "Ambrose"
+    },
+    "talker": "char2",
+    "say": ""
+  },
+  { //dialogue 14
+    "bg": "stanleytv",
+    "overlay": "trash",
+    "char1": {
+      "emotion": "ANGRY",
+      "name": "Kumani"
+    },
+    "char2": {
+      "emotion": "CRY",
+      "name": "Ambrose"
+    },
+    "talker": "char1",
+    "say": "stop it"
+  },
+  { //dialogue 14
+    "bg": "stanleytv",
+    "overlay": "trash",
+    "char1": {
+      "emotion": "ANGRY",
+      "name": "Kumani"
+    },
+    "char2": {
+      "emotion": "CRY",
+      "name": "Ambrose"
+    },
+    "talker": "char2",
+    "say": ""
+  },
+  { //dialogue 14
+    "bg": "stanleytv",
+    "overlay": "trash",
+    "char1": {
+      "emotion": "ANGRY",
+      "name": "Kumani"
+    },
+    "char2": {
+      "emotion": "CRY",
+      "name": "Ambrose"
+    },
+    "talker": "char1",
+    "say": "ok fine i will have pity sex with you"
+  },
+  { //dialogue 14
+    "bg": "stanleytv",
+    "overlay": "trash",
+    "char1": {
+      "emotion": "ANGRY",
+      "name": "Kumani"
+    },
+    "char2": {
+      "emotion": "IDLE",
+      "name": "Ambrose"
+    },
+    "talker": "char2",
+    "say": "ha ha HA"
+  },
+  { //dialogue 14
+    "bg": "stanleytv",
+    "overlay": "trash",
+    "char1": {
+      "emotion": "ANGRY",
+      "name": "Kumani"
+    },
+    "char2": {
+      "emotion": "IDLE",
+      "name": "Ambrose"
+    },
+    "talker": "char2",
+    "say": "Ambrose ALWAYs gets  what he wants aha ha "
+  },
+  { //dialogue 14
+    "bg": "stanleytv",
+    "overlay": "trash",
+    "char1": {
+      "emotion": "ANGRY",
+      "name": "Kumani"
+    },
+    "char2": {
+      "emotion": "IDLE",
+      "name": "Ambrose"
+    },
+    "talker": "char2",
+    "say": "ha"
+  },
+];
+const exit = "https://www.google.com/search?q=sadism+";
